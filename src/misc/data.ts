9d4cb15b06d0a6aeeafe3c11b3cf949bd4522235
tris <nexus4fortune@gmail.com>
2024-01-03 22:27:25 +0900

    暫定版ダミーデータ作成

---
+++ src/misc/data.ts
@@ -0,0 +1,36 @@
+import { ItemSet, PartsItem } from "./type";
+
+/** データファイルの型 */
+export type DataType = {
+  log: string[];
+  parts: PartsItem[];
+  sets: ItemSet[];
+};
+
+/** 試験用のダミーデータ用オブジェクト */
+export const DUMMY_DATA: DataType = {
+  log: [],
+  parts: [
+    {
+      id: "aaaaaaaaa",
+      name: "TEST Dummy F001",
+      description: "テスト用フレーム",
+      kind: "frame",
+      limit: undefined,
+    },
+    {
+      id: "bbbbbbbbb",
+      name: "Ultegraホイール 36mm",
+      description: "",
+      kind: "ホイール",
+    },
+    {
+      id: "ccccccccc",
+      name: "Continental GP5000S TR",
+      description: "",
+      kind: "前タイヤ",
+      limit: 5000,
+    },
+  ],
+  sets: [],
+};
--- src/misc/type.ts
+++ src/misc/type.ts
@@ -1,18 +1,51 @@
-import { PartsKind } from "./constants";
+/** パーツのID */
+export type PartsId = string;
 
 /**
  * パーツの情報を保持する型。
  * 永続化する情報だけとして、計算可能な値は含めない。
  */
 export type PartsItem = {
+  /** ユニークなID */
+  id: PartsId;
   /** 名前 */
   name: string;
   /** 詳細 */
   description: string;
   /** 種類 */
-  kind: PartsKind;
-  /** 累積距離。データとしては保持せず、必要に応じて計算する */
-  // distance: number;
+  kind: string;
   /** 使用限界距離 */
-  limit: number;
+  limit?: number;
+  /** アイコン */
+  // icon:string;
+};
+
+/**
+ * パーツのデータ。
+ * PartsItem に、計算した値を加えて保持する。
+ */
+export type PartsItemData = PartsItem & {
+  /** 累積距離 */
+  distance: number;
+  /** パーツが含まれるセットのリスト */
+  sets: SetId[];
+};
+
+// ---------------------------------------------------------------
+// セット関係
+// ---------------------------------------------------------------
+
+/** セットのID */
+export type SetId = string;
+
+/**
+ * セットの情報を保持する型
+ */
+export type ItemSet = {
+  /** ユニークなID */
+  id: SetId;
+  /** 名前 */
+  name: string;
+  /** 含むパーツの一覧 */
+  parts: PartsId[];
 };
