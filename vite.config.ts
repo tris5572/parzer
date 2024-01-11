b90775d3ce24dfad2f08947b7e852a3499baf211
tris <nexus4fortune@gmail.com>
2024-01-12 07:37:49 +0900

    デプロイ：テストで設定変更

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: process.env.GITHUB_PAGES ? "/parzer/" : "/",
+  base: "/parzer/",
+  // base: process.env.GITHUB_PAGES ? "/parzer/" : "/",
 });
