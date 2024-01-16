d3e796f126e1e402aefb17eb3fb107cc84699142
tris <nexus4fortune@gmail.com>
2024-01-16 21:43:53 +0900

    追加ボタンにスタイルを設定

--- src/components/common/AddButton.tsx
+++ src/components/common/AddButton.tsx
@@ -1,4 +1,25 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  background: ${(p) => p.theme.colors.key.dark};
+  padding: 0.5rem;
+  margin: 0.2rem;
+  color: white;
+  border-radius: 4px;
+  width: 1.6rem;
+  height: 1.5rem;
+  display: flex;
+  align-items: center;
+`;
+
+const L = styled(Link)`
+  text-decoration: none;
+  color: white;
+  text-align: center;
+  width: 100%;
+  font-weight: bold;
+`;
 
 type Props = {
   link: string;
@@ -6,8 +27,8 @@ type Props = {
 
 export function AddButton(props: Props) {
   return (
-    <div>
-      <Link to={props.link}>＋</Link>
-    </div>
+    <Wrapper>
+      <L to={props.link}>＋</L>
+    </Wrapper>
   );
 }
